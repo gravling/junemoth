791145528498a724d6f46532f1c0f5477070ba1a
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2018-10-09 15:49:57 +0200

    Added acronym file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { acronyms } from './acronyms.json';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            RS = ReSpons
-          </p>
-        </header>
-      </div>
-    );
-  }
+    getRandomAcronym() {
+        return acronyms[Math.floor(acronyms.length * Math.random())];
+    }
+    render() {
+        const { acronym, meaning } = this.getRandomAcronym();
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        {acronym} = {meaning}
+                    </p>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
