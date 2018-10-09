fb5fa5e2386aee6f06caa8869bbfb3c09e011f4d
Fredrik Lundberg <fredrik@ablu.se>
2018-10-09 22:30:37 +0200

    Salmonize and prettify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Logo from './Logo';
 import './App.css';
 import { acronyms } from './acronyms.json';
 
 class App extends Component {
-    getRandomAcronym() {
-        return acronyms[Math.floor(acronyms.length * Math.random())];
-    }
-    render() {
-        const { acronym, meaning } = this.getRandomAcronym();
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        {acronym} = {meaning}
-                    </p>
-                </header>
-            </div>
-        );
-    }
+  getRandomAcronym() {
+    return acronyms[Math.floor(acronyms.length * Math.random())];
+  }
+  render() {
+    const { acronym, meaning } = this.getRandomAcronym();
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Logo color="salmon" className="App-logo" alt="logo" />
+          <p>
+            {acronym} = {meaning}
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
